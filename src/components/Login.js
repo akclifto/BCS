acad386b91c5bada25a5c8ab178603db0e8fe799
akclifto <akclifto@asu.edu>
2021-02-17 12:16:07 -0700

    login working finally

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,45 +1,71 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import history from './history';
 
-export default () => (
-    <div className="container">
-        <div className="login" align="center">
-            <h1>Login</h1>
-            <div className="login__emailPass">
-                <p>
-                    <input
-                        type="text"
-                        name="login"
-                        value=""
-                        placeholder="Username or Email"
-                    />
-                </p>
-                <p>
-                    <input
-                        type="password"
-                        name="password"
-                        value=""
-                        placeholder="Password"
-                    />
-                </p>
-                <p className="submit__login">
-                    <input
-                        type="submit"
-                        name="submitButton"
-                        value="Login"
-                    />
-                </p>
-            </div>
 
-            <div className="help-login">
-                <div className="help-login__links">
-                    <p>
-                        <a href="#">Forgot password?</a>
-                    </p>
-                    <p>
-                    <a href="#">New User?  Sign up here.</a>
-                    </p>
+
+export default class Login extends React.Component {
+
+    state = {
+        holder_email: undefined,
+        holder_pass: undefined,
+    }
+
+    onSubmit_login = (e) => {
+        history.push("/");
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="login">
+                    <h1>Account Login</h1>
+                    <div className="login__emailPass">
+                        <form
+                            method="post"
+                            action="">
+                            <p>
+                                <input
+                                    type="text"
+                                    name="login"
+                                    value={this.holder_email}
+                                    placeholder="Username or Email"
+                                />
+                            </p>
+                            <p>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    value={this.holder_pass}
+                                    placeholder="Password"
+                                />
+                            </p>
+                            </form>
+                            <form className="form__button">
+                                <p className="login___button">
+                                    <button
+                                        type="submit"
+                                        name="login_button"
+                                        value="Login"
+                                        onClick={this.onSubmit_login}
+                                    > Login
+                                        </button>
+                                </p>
+                            </form>
+                    </div>
+
+                    <div className="help-login">
+                        <div className="help-login__links">
+                            <p>
+                                <a href="#">Forgot password?</a>
+                            </p>
+                            <p>
+                                <a href="#">New User?  Sign up here.</a>
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-);
+        );
+    }
+};
