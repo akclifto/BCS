e175fb68c7c0997adc4dafc8d8935780d98f26d8
akclifto <akclifto@asu.edu>
2021-02-18 12:31:16 -0700

    style for header wip

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => (
-    <header>
-        <h1>Best Community Services</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}> Dashboard</NavLink>  |
-        <NavLink to="/services" activeClassName="is-active"> Services</NavLink>  |
-        <NavLink to="/volunteer" activeClassName="is-active"> Volunteer</NavLink>  |
-        <NavLink to="/donate" activeClassName="is-active"> Donate</NavLink>  |
-        <NavLink to="/login" activeClassName="is-active"> Login</NavLink>  |
-    </header>
-
+    <div className="header__container">
+        <div className="header">
+            <a href="/" className="header__company">Best Community Services</a>
+            <div className="header__nav">
+                <NavLink to="/" activeClassName="is-active" exact={true} className="nav"> Dashboard</NavLink>
+                <NavLink to="/services" activeClassName="is-active" className="nav"> Services</NavLink>
+                <NavLink to="/volunteer" activeClassName="is-active" className="nav"> Volunteer</NavLink>
+                <NavLink to="/donate" activeClassName="is-active" className="nav"> Donate</NavLink>
+                <NavLink to="/login" activeClassName="is-active" className="nav"> Login</NavLink>
+            </div>
+        </div>
+    </div>
 );
 
 export default Header;
