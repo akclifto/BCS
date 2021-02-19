abb59db745af328bdda38e93a7de4f6fb6fbb832
akclifto <akclifto@asu.edu>
2021-02-19 11:28:10 -0700

    update webpack ts heroku

--- src/pages/BCSDashboard.js
+++ src/pages/BCSDashboard.js
@@ -36,7 +36,7 @@ const BCSDashboard = () => (
         <div className="dash">
             <div className="dash__component">
                 <div className="dash__component-header">
-                    <a></a>Services
+                    <a href="#services" className="dash__component-title">Services</a>
                 </div>
                 <div className="dash__list">
                     <ul className="dash__list--headline">
