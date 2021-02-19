ed4e2c2983418c3c5e384ac1780651957d488a32
akclifto <akclifto@asu.edu>
2021-02-19 11:26:33 -0700

    update webpack ts heroku

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import AppRouter from './routes/router.js';
+import AppRouter from './routes/router';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
