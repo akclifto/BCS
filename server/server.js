0fb4a0360b0c9dffbb98e8bf3fb0ef23df230077
akclifto <akclifto@asu.edu>
2021-02-18 17:02:29 -0700

    update server.js trying to ts heroku

--- server/server.js
+++ server/server.js
@@ -2,15 +2,28 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+ // __dirname is the relative source directory for this file (/server/*)
 const publicPath = path.join(__dirname, '..', 'public', 'dist');
 const port = process.env.PORT || 3000;
 
 // middleware:  some code that runs for each request. set it up with app.use();
 app.use(express.static(publicPath));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(publicPath, 'index.html'));
+// });
+let protected = ["favicon.ico"];
+
+app.get("*", (req, res) => {
+  
+    if (protected.includes(publicPath)) {
+      // Return the actual file
+      res.sendFile(path.join(publicPath, '../public'));
+    } else {
+      // Otherwise, redirect to /public/index.html
+      res.sendFile(path.join(publicPath, 'index.html'));
+    }
+  });
 
 // start the server locally
 app.listen(port, () => {
