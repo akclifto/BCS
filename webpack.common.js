ecd2ab23d19192b7630d912efd6961ad85a7c4de
akclifto <akclifto@asu.edu>
2021-02-19 10:13:29 -0700

    update webpack ts heroku

--- webpack.common.js
+++ webpack.common.js
@@ -3,9 +3,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    app: './src/app.js',
-  },
+  entry: './src/app.js',
   module: {
     rules: [{
       loader: 'babel-loader',
