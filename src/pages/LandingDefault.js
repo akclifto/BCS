f94a3c78ec9489dc8045e8a3da2b34f7b1394617
akclifto <akclifto@asu.edu>
2021-02-22 19:18:15 -0700

    make default landing

--- src/pages/LandingDefault.js
+++ src/pages/LandingDefault.js
@@ -3,11 +3,30 @@ import React from 'react';
 const LandingDefault = () => {
 
     return (
-        <div>
-            This is the default landing page!
+        <div className="landingDefault__container">
+
+            <div className="landingDefault">
+
+                <div className="landingDefault__left">
+                    <div className="landingDefault__greet">
+                        <p> Make a difference in <br/>                      
+                            Your Community</p>
+                    </div>
+                </div>
+
+                <div className="landingDefault__right">
+                    right div pic to go here
+
+                    <div className="landingDefault__signup">
+                        sign up box here
+                    </div>
+
+                </div>
+            </div>
+
         </div>
     )
-    
+
 }
 
 export default LandingDefault;
