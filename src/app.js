d5ce211de74ec64de4bd0ee664cb5e6e1775314f
akclifto <akclifto@asu.edu>
2021-02-23 13:21:25 -0700

    webpack heroku ts

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
-import AppRoutes from "./router/routes";
+import AppRouter from "./routes/router";
 
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV !== "production") {
 
 const App = () => (
   <BrowserRouter>
-    <AppRoutes />
+    <AppRouter />
   </BrowserRouter>
 );
 
--- webpack.common.js
+++ webpack.common.js
@@ -49,12 +49,14 @@ module.exports = {
       components: path.resolve(__dirname, "src/components"),
       containers: path.resolve(__dirname, "src/containers"),
       pages: path.resolve(__dirname, "src/pages"),
+      resources: path.resolve(__dirname, "src/resources"),
+      routes: path.resolve(__dirname, "src/routes"),
+      styles: path.resolve(__dirname, "src/styles"),
+      
       // browser: path.resolve(
       //   __dirname,
       //   "node_modules/@types/react-router-dom/index.d.ts"
       // ),
-      routes: path.resolve(__dirname, "src/routes"),
-      styles: path.resolve(__dirname, "src/styles"),
     },
   },
   plugins: [
